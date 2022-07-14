47a17679051b0cd473c0307f539f8595fdc24c3b
mconneely-sb <84188442+mconneely-sb@users.noreply.github.com>
2022-07-14 16:41:28 +0100

    Update product.js

--- src/product/product.js
+++ src/product/product.js
@@ -2,6 +2,9 @@ class Product {
     constructor(id, type, name, version, price) {
         this.id = id;
         this.type = type;
+        this.name = name;
+        this.version = version;
+        this.price = price;
     }
 }
 
